1eecff69b8ab75f9ce5e58ae6737a1185fd2a312
kirtisharma1111 <kirtisharma132003@gmail.com>
2025-02-19 14:23:05 +0530

    new file

---
+++ Complete_Java/src/basics/Typecasting.java
@@ -0,0 +1,12 @@
+package basics;
+import java.util.Scanner;
+public class Typecasting {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		double y = (double)n;
+		System.out.print(y/2);
+
+	}
+
+}
